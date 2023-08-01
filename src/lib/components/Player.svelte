5f75222d36a937d15c7418563b21f895bb55cf18
GeorgeTheSlayer <holland@sersen.net>
2023-08-01 17:32:32 -0500

    Changed sound loading

--- src/lib/components/Player.svelte
+++ src/lib/components/Player.svelte
@@ -5,9 +5,3 @@
 <div class="lg:bottom-0 bottom-16 inset-x-0 fixed bg-secondary-background h-16 lg:h-20 flex">
 	<img src={imgFile} alt="" class="p-4" />
 </div>
-
-<style lang="postcss">
-	.square {
-		aspect-ratio: 1 / 1;
-	}
-</style>
